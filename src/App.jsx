d606e0f5c45286bf49fc69c552fb0c96f0d80017
sunnatjon <sunnattoxtaqulov4444@gmail.com>
2024-06-05 08:16:53 +0300

    Add new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './Pages/LoginPage/LoginPage'
+import HomePages from './Pages/HomePages/HomePages'
 
 const App = () => {
-  const [raqam, setRaqam] = useState('')
-  const [parol, setParol] = useState('')
-  console.log(raqam , parol);
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-    fetch('https://autoapi.dezinfeksiyatashkent.uz/api/categories', {
-      method: 'POST',
-      body: JSON.stringify({
-        phone_number : raqam,
-        password : parol
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-       .then((response) => response.json())
-       .then((data) => {
-          console.log(data);
-          // Handle data
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
-  }
+
   return (
-    <div>
-      <input type="text" onChange={(e) =>setRaqam(e?.target?.value)} />
-      <input type="text" onchange={(e)=>setParol(e?.target?.value)}/>
-      <button onClick={handleSubmit}>yubor</button>
-    </div>
+<>
+<Routes>
+ <Route path="/" element={<LoginPage/>} />
+  <Route path='/home' element={<HomePages/>} />
+</Routes>
+<ToastContainer />
+</>
+
   )
 }
+
 
 export default App
---
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const LoginPage = () => {
+  const [raqam, setRaqam] = useState('')
+  const [parol, setParol] = useState('')
+  const navigate = useNavigate()
+  // console.log(raqam , parol);
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    fetch('https://autoapi.dezinfeksiyatashkent.uz/api/auth/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        phone_number : raqam,
+        password : parol
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data?.data?.tokens?.accessToken?.token);
+          if(data?.success == true){
+            toast.success(data?.message)
+            localStorage.setItem( "accesstoken" , data?.data?.tokens?.accessToken?.token)
+            navigate('/home')
+          }else{
+            toast.error(data?.message)
+            navigate('/')
+          }
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+  }
+  return (
+    <div>
+      <input type="text" onChange={(e) =>setRaqam(e?.target?.value)} />
+      <input type="text" onChange={(e) =>setParol(e?.target?.value)}/>
+      <button onClick={handleSubmit}>Login qilish </button>
+
+
+    </div>
+  )
+}
+
+export default LoginPage
